02a60b6e11c21b1c34e47fcec2bc1ffcd703f0ff
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-02-28 14:22:24 +0000

    add lib utils Components and asset folders

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,20 +1,20 @@
 import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-    return {
-        name: "HedgeHog Investments",
-        short_name: "HedgeHog",
-        description: "Smart management of your investment portfolio.",
-        start_url: "https://",
-        display: "standalone",
-        background_color: "#22287F",
-        theme_color: "#fff",
-        icons: [
-            {
-                src: "/favicon.ico",
-                sizes: "any",
-                type: "image/x-icon",
-            },
-        ],
-    };
+  return {
+    name: "HedgeHog Investments",
+    short_name: "HedgeHog",
+    description: "Smart management of your investment portfolio.",
+    start_url: "https://",
+    display: "standalone",
+    background_color: "#22287F",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "assets/icons/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
 }
