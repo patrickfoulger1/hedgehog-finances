097ea92a12973a3834e7b6004f59d433243b3554
Drew <drew@werd.uk>
2025-03-04 16:24:49 +0000

    add lock check before rendering inbox

--- src/app/inbox/components/inbox.tsx
+++ src/app/inbox/components/inbox.tsx
@@ -52,31 +52,34 @@ export function NovuInbox({ watchlist }) {
             },
         },
     };
-
-    return (
-        <div className="bg-gray-300 rounded-lg w-full">
-            <Inbox
-                applicationIdentifier={"" + process.env.NEXT_PUBLIC_NOVU_APP_ID}
-                subscriberId="4a06f9ce-94f1-4b9d-b60f-c8b22d2810c9" // needs to come from the user session
-                routerPush={(path: string) => router.push(path)}
-                tabs={tabs}
-                appearance={appearance}>
-                <Bell
-                    renderBell={(unreadCount) => {
-                        if (unreadCount > 0) {
-                            return (
-                                <div className="flex justify-center align-center h-6 aspect-square rounded-full bg-red-500 p-1 text-white">
-                                    <span className="relative -top-1">{unreadCount}</span>
-                                </div>
-                            );
-                        } else {
-                            return null;
-                        }
-                    }}
-                />
-                <Notifications />
-                <Preferences />
-            </Inbox>
-        </div>
-    );
+    if ("locks" in navigator) {
+        return (
+            <div className="bg-gray-300 rounded-lg w-full">
+                <Inbox
+                    applicationIdentifier={"" + process.env.NEXT_PUBLIC_NOVU_APP_ID}
+                    subscriberId="4a06f9ce-94f1-4b9d-b60f-c8b22d2810c9" // needs to come from the user session
+                    routerPush={(path: string) => router.push(path)}
+                    tabs={tabs}
+                    appearance={appearance}>
+                    <Bell
+                        renderBell={(unreadCount) => {
+                            if (unreadCount > 0) {
+                                return (
+                                    <div className="flex justify-center align-center h-6 aspect-square rounded-full bg-red-500 p-1 text-white">
+                                        <span className="relative -top-1">{unreadCount}</span>
+                                    </div>
+                                );
+                            } else {
+                                return null;
+                            }
+                        }}
+                    />
+                    <Notifications />
+                    <Preferences />
+                </Inbox>
+            </div>
+        );
+    } else {
+        console.log("Weblocks API not supported in this broswer");
+    }
 }
