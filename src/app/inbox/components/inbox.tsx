1e054e8605adc777aa11a5de29343e5a659df1aa
Drew <drew@werd.uk>
2025-03-05 16:47:49 +0000

    fix hydration error

--- src/app/inbox/components/inbox.tsx
+++ src/app/inbox/components/inbox.tsx
@@ -38,50 +38,50 @@ export function NovuInbox({ watchlist }: { watchlist: any }) {
         });
     }, []);
 
-    const appearance = {
-        elements: {
-            bellIcon: {
-                padding: "1rem",
-                backgroundColor: "white",
-                borderRadius: "50%",
-            },
-            notification: {
-                backgroundColor: "white",
-                borderRadius: "0.5rem",
-                boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
-            },
-            preferencesContainer: {
-                borderRadius: "0.5rem",
-                margin: "10px",
-            },
-        },
-    };
     if ("locks" in navigator) {
         return (
-            <div className="bg-gray-300 rounded-lg w-full">
-                <Inbox
-                    applicationIdentifier={"" + process.env.NEXT_PUBLIC_NOVU_APP_ID}
-                    subscriberId="4a06f9ce-94f1-4b9d-b60f-c8b22d2810c9" // needs to come from the user session
-                    routerPush={(path: string) => router.push(path)}
-                    tabs={tabs}
-                    appearance={appearance}>
-                    <Bell
-                        renderBell={(unreadCount) => {
-                            if (unreadCount > 0) {
-                                return (
-                                    <div className="flex justify-center align-center h-6 aspect-square rounded-full bg-red-500 p-1 text-white">
-                                        <span className="relative -top-1">{unreadCount}</span>
-                                    </div>
-                                );
-                            } else {
-                                return null;
-                            }
-                        }}
-                    />
-                    <Notifications />
-                    <Preferences />
-                </Inbox>
-            </div>
+            <Inbox
+                applicationIdentifier={"" + process.env.NEXT_PUBLIC_NOVU_APP_ID}
+                subscriberId="4a06f9ce-94f1-4b9d-b60f-c8b22d2810c9" // needs to come from the user session
+                routerPush={(path: string) => router.push(path)}
+                tabs={tabs}
+                preferencesFilter={{ tags: ["general", "admin", "security"] }}
+                appearance={{
+                    variables: {
+                        colorBackground: "#ebebeb",
+                        colorForeground: "#1a181f",
+                        colorPrimary: "#0b71ca",
+                        colorPrimaryForeground: "#ffffff",
+                        colorSecondary: "#F3F3F3",
+                        colorSecondaryForeground: "#1A1523",
+                        colorCounter: "#E5484D",
+                        colorCounterForeground: "white",
+                        colorNeutral: "black",
+                        fontSize: "inherit",
+                        borderRadius: "0.375rem",
+                    },
+                    elements: {
+                        inboxContent: {
+                            backgroundColor: "#fff",
+                        },
+                    },
+                }}>
+                <Bell
+                    renderBell={(unreadCount) => {
+                        if (unreadCount > 0) {
+                            return (
+                                <div className="flex justify-center align-center h-6 aspect-square rounded-full bg-red-500 p-1 text-white">
+                                    <span className="relative -top-1">{unreadCount}</span>
+                                </div>
+                            );
+                        } else {
+                            return null;
+                        }
+                    }}
+                />
+                <Notifications />
+                <Preferences />
+            </Inbox>
         );
     } else {
         console.log("Weblocks API not supported in this broswer");
