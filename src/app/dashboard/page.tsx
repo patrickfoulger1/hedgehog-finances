7a4b9e011002b623ba4898364ada530cd79072f9
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-03-05 15:49:18 +0000

    fix access-control-allow-origin error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,26 +5,25 @@ import { Session } from "next-auth";
 import { prisma } from "@/lib/db";
 import { User } from "@prisma/client";
 import Charts from "./charts";
-import { Button } from "@/components/ui/button";
 export default async function DashboardPage() {
-    const session = (await getServerSession(authOptions)) as Session;
-    const user = (await prisma.user.findUnique({
-        where: {
-            email: session.user.email,
-        },
-    })) as User;
+  const session = (await getServerSession(authOptions)) as Session;
+  const user = (await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  })) as User;
 
-    const stocks = await prisma.watchlist.findMany({
-        where: {
-            userId: user.id, //will error on anyone who signed up with google
-        },
-    });
+  const stocks = await prisma.watchlist.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
 
-    return (
-        <>
-            <Header user={user} />
-            <h1>Welcome back!</h1>
-            <Charts stocks={stocks}></Charts>
-        </>
-    );
+  return (
+    <>
+      <Header user={user} />
+      <h1>Welcome back!</h1>
+      <Charts stocks={stocks}></Charts>
+    </>
+  );
 }
