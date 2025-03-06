5fcba46985ea155f5ba136d46660adbb982d8fd0
Drew <drew@werd.uk>
2025-03-06 12:18:08 +0000

    add Suspense to registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,13 @@
 import { RegisterForm } from "./RegisterForm";
-
+import { Suspense } from "react";
 import AuthPage from "@/components/AuthPage";
 
 export default function RegisterPage() {
-  return (
-    <AuthPage>
-      <RegisterForm />
-    </AuthPage>
-  );
+    return (
+        <AuthPage>
+            <Suspense fallback={<>Loading...</>}>
+                <RegisterForm />
+            </Suspense>
+        </AuthPage>
+    );
 }
