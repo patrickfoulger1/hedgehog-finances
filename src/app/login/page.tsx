36a1156c3caca1bad63a4820c5407e5d9ff3750b
Drew <drew@werd.uk>
2025-03-06 12:08:50 +0000

    add Suspense around login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import { LoginForm } from "@/app/login/login-form";
+import { Suspense } from "react";
 import AuthPage from "@/components/AuthPage";
 
 export default function LoginPage() {
-  return (
-    <AuthPage>
-      <LoginForm />
-    </AuthPage>
-  );
+    return (
+        <AuthPage>
+            <Suspense fallback={<>Loading...</>}>
+                <LoginForm />
+            </Suspense>
+        </AuthPage>
+    );
 }
