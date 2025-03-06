b6079705e76b2d5ce3b19253004ff7b302ab1eaf
Drew <drew@werd.uk>
2025-03-06 13:58:06 +0000

    re-add suspense rendering

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import { LoginForm } from "@/app/login/login-form";
 import AuthPage from "@/components/AuthPage";
+import { Suspense } from "react";
 
 export default function LoginPage() {
-  return (
-    <AuthPage>
-      <LoginForm />
-    </AuthPage>
-  );
+    return (
+        <AuthPage>
+            <Suspense fallback={<>Loading</>}>
+                <LoginForm />
+            </Suspense>
+        </AuthPage>
+    );
 }
