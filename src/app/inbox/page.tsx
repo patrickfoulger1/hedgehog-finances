6fe2fda8b53bcfa1c204090c21e23a019f6c0d6a
Drew <drew@werd.uk>
2025-03-06 10:04:47 +0000

    add header to inbox

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/header";
+import { User } from "@prisma/client";
 import { NovuInbox } from "./components/inbox";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
@@ -6,23 +8,22 @@ import { prisma } from "@/lib/db";
 
 export default async function Notifications() {
     const session = (await getServerSession(authOptions)) as Session;
-    const watchlist = await prisma.user
-        .findUnique({
-            where: {
-                email: session.user.email,
-            },
-        })
-        .then((user) => {
-            return prisma.watchlist.findMany({
-                where: {
-                    userId: user?.id,
-                },
-            });
-        });
+
+    const user = (await prisma.user.findUnique({
+        where: {
+            email: session.user.email,
+        },
+    })) as User;
+
+    const watchlist = await prisma.watchlist.findMany({
+        where: {
+            userId: user?.id,
+        },
+    });
 
     return (
         <div className="p-5">
-            <h1>Inbox for: {session.user.email}</h1>
+            <Header user={user} />
             <NovuInbox watchlist={watchlist} />
         </div>
     );
