738be87d60a6f44a2ffaba460bd6a023107aaf7e
Drew <drew@werd.uk>
2025-03-10 23:35:12 +0000

    move to environment variables

--- src/app/api/firebase-config/route.ts
+++ src/app/api/firebase-config/route.ts
@@ -1,11 +1,11 @@
 async function handler(req: Request) {
     return Response.json({
-        apiKey: "AIzaSyBEVzdMO2RQhzE9mPlsif-TkrUH51PPYW4",
-        authDomain: "web-notifications-d9bf1.firebaseapp.com",
-        projectId: "web-notifications-d9bf1",
-        storageBucket: "web-notifications-d9bf1.firebasestorage.app",
-        messagingSenderId: "1070244670584",
-        appId: "1:1070244670584:web:ef2dcf096eb17457b6d18d",
+        apiKey: `${process.env.NEXT_PUBLIC_FIREBASEAPIKEY}`,
+        authDomain: `${process.env.NEXT_PUBLIC_PROJECTID}.firebaseapp.com`,
+        projectId: `${process.env.NEXT_PUBLIC_PROJECTID}`,
+        storageBucket: `${process.env.NEXT_PUBLIC_PROJECTID}.firebasestorage.app`,
+        messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGE_SENDER}`,
+        appId: `${process.env.NEXT_PUBLIC_NOTIFY_APPID}`,
     });
 }
 
