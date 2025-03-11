428dcf77c3ee5fd6ef071ba4e6af6a5935256887
Drew <drew@werd.uk>
2025-03-11 10:55:07 +0000

    move contact pref object into partent component

--- src/app/alerts/page.tsx
+++ src/app/alerts/page.tsx
@@ -3,6 +3,7 @@ import { User, Watchlist, ContactPreferences } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 import { Session } from "next-auth";
+import { getUserContactPrefs } from "@/serverActions";
 import { prisma } from "@/lib/db";
 import IosInstall from "./components/iOSInstall";
 import PreferenceTable from "./components/preferenceTable";
@@ -16,12 +17,26 @@ export default async function Notifications() {
         },
     })) as User;
 
+    const prefObject = getUserContactPrefs(user.id).then((prefs) => {
+        const finalObj = {};
+        prefs.forEach((pref) => {
+            for (const [key, value] of Object.entries(pref)) {
+                const concatKey = [pref.stockSymbol, key].join("-");
+                if (["push", "email", "inApp"].includes(key)) {
+                    finalObj[concatKey] = value;
+                }
+            }
+        });
+
+        return finalObj;
+    });
+
     return (
         <div className="p-5">
             <Header user={user} />
             <p>{user.id}</p>
             <IosInstall />
-            <PreferenceTable user={user} />
+            <PreferenceTable user={user} prefObject={prefObject} />
         </div>
     );
 }
