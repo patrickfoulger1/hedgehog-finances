75b8772de7927e3a37cd6d467f05b545d6509e20
Drew <drew@werd.uk>
2025-03-11 10:53:13 +0000

    add toaster to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,11 +12,10 @@ import { useState, useEffect, useMemo } from "react";
 import { User } from "@/lib/types";
 import { signOut } from "next-auth/react";
 import { unreadCount } from "@/serverActions";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 
 export default function Header({ user }: { user: User }) {
-    const app = initializeApp(firebaseConfig);
-    const messaging = getMessaging(app);
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [currentPage, setCurrentPage] = useState("");
     const [unreadItems, setUnreadItems] = useState(0);
@@ -27,6 +26,8 @@ export default function Header({ user }: { user: User }) {
     };
 
     useEffect(() => {
+        const app = initializeApp(firebaseConfig);
+        const messaging = getMessaging(app);
         setCurrentPage(window.location.href);
         unreadCount(user.id).then((c) => {
             setUnreadItems(c);
@@ -42,63 +43,82 @@ export default function Header({ user }: { user: User }) {
                 });
             }
         });
+
+        navigator.serviceWorker.addEventListener("message", (event) => {
+            if (event.data && event.data.type === "PUSH") {
+                console.log("Message received from service worker:", event.data.payload);
+                toast(event.data.payload.notification.title, { description: event.data.payload.notification.body, action: { label: "View", onClick: () => (window.location.href = "/stocks/MSFT") } });
+            }
+        });
+
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker
+                .register("/firebase-messaging-sw.js")
+                .then((registration) => {})
+                .catch((error) => {
+                    console.error("Service Worker registration failed:", error);
+                });
+        }
     }, []);
 
     const defaultProfilePicture = useMemo(() => (currentPage.includes("stocks") ? "../media/profile-image.png" : "media/profile-image.png"), [currentPage]);
 
     return (
-        <header>
-            <button
-                className="menu-controller"
-                onClick={() => {
-                    setIsMenuOpen(!isMenuOpen);
-                }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                    <path
-                        fillRule="evenodd"
-                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                    />
-                </svg>
-                {isMenuOpen ? <MobileMenu /> : null}
-            </button>
-            <nav className="flex">
-                <Link href="/dashboard">Dashboard</Link>
-                <Link href="/inbox" className="flex flex-nowrap gap-2">
-                    Inbox
-                    {unreadItems ? <p className="bg-red-500 rounded-full aspect-square w-5 h-5 align-middle text-center text-sm">{unreadItems}</p> : null}
-                </Link>
-            </nav>
-            <div className="search-account-area pe-2 gap-4">
-                <SearchBar />
+        <>
+            <Toaster />
+            <header>
+                <button
+                    className="menu-controller"
+                    onClick={() => {
+                        setIsMenuOpen(!isMenuOpen);
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                        <path
+                            fillRule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                        />
+                    </svg>
+                    {isMenuOpen ? <MobileMenu /> : null}
+                </button>
+                <nav className="flex">
+                    <Link href="/dashboard">Dashboard</Link>
+                    <Link href="/inbox" className="flex flex-nowrap gap-2">
+                        Inbox
+                        {unreadItems ? <p className="bg-red-500 rounded-full aspect-square w-5 h-5 align-middle text-center text-sm">{unreadItems}</p> : null}
+                    </Link>
+                </nav>
+                <div className="search-account-area pe-2 gap-4">
+                    <SearchBar />
 
-                <DropdownMenu>
-                    <DropdownMenuTrigger className="px-3 py-2 rounded-md border-1 border-white/30">
-                        <div className="fullname">{user.username}</div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>
-                            <Link href="/account">My Account</Link>
-                        </DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Link href="/alerts">Alerts</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Button
-                                className={"logoutButton"}
-                                onClick={(e) => {
-                                    handleLogout(e);
-                                }}>
-                                Logout
-                            </Button>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                <Link href="/account" className="account">
-                    <img src={user.image ? user.image : defaultProfilePicture} alt="profile picture" />
-                </Link>
-            </div>
-        </header>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="px-3 py-2 rounded-md border-1 border-white/30">
+                            <div className="fullname">{user.username}</div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>
+                                <Link href="/account">My Account</Link>
+                            </DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Link href="/alerts">Alerts</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Button
+                                    className={"logoutButton"}
+                                    onClick={(e) => {
+                                        handleLogout(e);
+                                    }}>
+                                    Logout
+                                </Button>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <Link href="/account" className="account">
+                        <img src={user.image ? user.image : defaultProfilePicture} alt="profile picture" />
+                    </Link>
+                </div>
+            </header>
+        </>
     );
 }
