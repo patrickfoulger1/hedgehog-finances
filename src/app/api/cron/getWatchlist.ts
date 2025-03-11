acab45e48c6f40f481f73ce0be22aab50c747db4
Drew <drew@werd.uk>
2025-03-11 15:26:34 +0000

    connects cron jobs with notifications

--- src/app/api/cron/getWatchlist.ts
+++ src/app/api/cron/getWatchlist.ts
@@ -1,15 +1,23 @@
+import { PrismaClient, Watchlist } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 export default async function getWatchlist() {
-    const stockSymbolArray: string[] = []; // Initialize an empty stock symbol array.
-    const response = await fetch("http://localhost:5000/Pinglist"); // Fetch the data from the Watchlist in DB.
-    const parsedPinglist = await response.json(); // Parse the Watchlist data.
+    const watchlist = await prisma.watchlist.findMany();
 
-    parsedPinglist.forEach((user) => {
-        user.stockSymbols.forEach((stock) => { // Iterate through stockSymbols array of each user.
-            if (!stockSymbolArray.includes(stock.symbol)) { // Check if the symbol is already in the array.
-                stockSymbolArray.push(stock.symbol); // Push the symbol to the array.
-            }
-        });
-    });
+    // const stockSymbolArray: string[] = []; // Initialize an empty stock symbol array.
+    // const response = await fetch("http://localhost:5000/Pinglist"); // Fetch the data from the Watchlist in DB.
+    // const parsedPinglist = await response.json(); // Parse the Watchlist data.
 
-    return stockSymbolArray;
+    // parsedPinglist.forEach((user) => {
+    //     user.stockSymbols.forEach((stock) => {
+    //         // Iterate through stockSymbols array of each user.
+    //         if (!stockSymbolArray.includes(stock.symbol)) {
+    //             // Check if the symbol is already in the array.
+    //             stockSymbolArray.push(stock.symbol); // Push the symbol to the array.
+    //         }
+    //     });
+    // });
+
+    return watchlist;
 }
