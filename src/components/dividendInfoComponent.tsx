5e5465a707510939f0e5d0aef8592d3f399dbef7
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-03-11 10:44:46 +0000

    cosmetic touch ups on dividend info

--- src/components/dividendInfoComponent.tsx
+++ src/components/dividendInfoComponent.tsx
@@ -7,27 +7,27 @@ export default function DividendInfoComponent({
 }) {
   if (dividendinfo?.length > 0) {
     return (
-      <div className="border rounded-2xl shadow-lg py-4 px-6 w-fit mx-auto my-4 text-center">
+      <div className="border rounded-2xl shadow-lg  px-6 pb-2 w-fit mx-auto my-4 text-center">
         <h2>
           <b>{dividendinfo[0]?.symbol}</b>
         </h2>
         <div className="flex flex-col sm:flex-row">
           Next dividend payment:{" "}
-          <span className="text-blue-700">{dividendinfo[0]?.date}</span>
+          <span className="text-blue-700 ml-2 dark:text-blue-200">{dividendinfo[0]?.date}</span>
         </div>
         <div className="flex flex-col sm:flex-row">
           Amount per share:{" "}
-          <span className="text-blue-700">
+          <span className="text-blue-700 ml-2 dark:text-blue-200">
             {String(dividendinfo[0]?.dividend)}${" "}
           </span>
         </div>
         <div className="flex flex-col sm:flex-row">
           Dividend frequency:{" "}
-          <span className="text-blue-700">{dividendinfo[0]?.frequency}</span>
+          <span className="text-blue-700 ml-2 dark:text-blue-200">{dividendinfo[0]?.frequency}</span>
         </div>
         <div className="flex flex-col sm:flex-row">
           You must hold shares before:{" "}
-          <span className="text-blue-700">{dividendinfo[0]?.recordDate}</span>
+          <span className="text-blue-700 ml-2 dark:text-blue-200">{dividendinfo[0]?.recordDate}</span>
         </div>
       </div>
     );
