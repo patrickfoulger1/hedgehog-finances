bc5dc862aa3dc2edfc1509513c9c7c9d8da4991b
Drew <drew@werd.uk>
2025-03-12 13:38:48 +0000

    add shadow bg to search bar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -3,39 +3,38 @@ import { useEffect, useState } from "react";
 import { api, getStockList } from "@/utils/frontendApiConfig";
 import SearchResults from "./searchResults";
 export default function SearchBar() {
-  const [stockList, setStockList] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [showResults, setShowResults] = useState(false);
+    const [stockList, setStockList] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [showResults, setShowResults] = useState(false);
 
-  useEffect(() => {
-    getStockList().then(({ data: data }) => {
-      setStockList(data);
-    });
-  }, []);
+    useEffect(() => {
+        getStockList().then(({ data: data }) => {
+            setStockList(data);
+        });
+    }, []);
 
-  return (
-    <div className="searchBarArea">
-      <Input
-        id="searchStocks"
-        value={searchQuery}
-        placeholder="Search stocks by company name"
-        onChange={async (e) => {
-          if (!stockList.length) {
-            setShowResults(true);
-          }
-          setShowResults(true);
-          setSearchQuery(e.currentTarget.value);
-        }}
-        onBlur={() => {
-          setTimeout(() => {
-            setSearchQuery("");
-            setShowResults(false);
-          }, 300);
-        }}
-      />
-      {showResults ? (
-        <SearchResults searchQuery={searchQuery} stockList={stockList} />
-      ) : null}
-    </div>
-  );
+    return (
+        <div className="searchBarArea">
+            <Input
+                id="searchStocks"
+                className="bg-white/10 inset-shadow-xs inset-shadow-white/10"
+                value={searchQuery}
+                placeholder="Search stocks by company name"
+                onChange={async (e) => {
+                    if (!stockList.length) {
+                        setShowResults(true);
+                    }
+                    setShowResults(true);
+                    setSearchQuery(e.currentTarget.value);
+                }}
+                onBlur={() => {
+                    setTimeout(() => {
+                        setSearchQuery("");
+                        setShowResults(false);
+                    }, 300);
+                }}
+            />
+            {showResults ? <SearchResults searchQuery={searchQuery} stockList={stockList} /> : null}
+        </div>
+    );
 }
